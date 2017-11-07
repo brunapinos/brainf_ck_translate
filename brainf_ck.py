9506866e0303bed7162baf7a8e354045c1da1a66
Guilherme Augusto <guilherme.francais@gmail.com>
2017-11-07 17:01:08 -0200

    [FIX] Fixes bugs on the translate

--- brainf_ck.py
+++ brainf_ck.py
@@ -4,49 +4,6 @@ from getch import getche
 import click
 
 ctx = SimpleNamespace(tokens=[], indent=1)
-code_p = SimpleNamespace(tokens=[], indent=1)
-
-def n(code_p, source):
-
-    data = [0]
-    ptr = 0
-    code_ptr = 0
-    breakpoints = []
-
-    while code_ptr < len(source):
-        cmd = source[code_ptr]
-
-        if cmd == '+':
-            data[ptr] = (data[ptr] + 1) % 256
-            x = ptr
-            code_p.tokens.append('    ' * code_p.indent + 'data[' + str(x) + '] += 1;\n')
-        elif cmd == '-':
-            data[ptr] = (data[ptr] - 1) % 256
-            x = ptr
-            code_p.tokens.append('    ' * code_p.indent + 'data[' + str(x) + '] -= 1;\n')
-        elif cmd == '>':
-            ptr += 1
-            code_p.tokens.append('    ' * code_p.indent + 'ptr++;\n')
-            if ptr >= len(data):
-                data.append(0)
-        elif cmd == '<':
-            ptr -= 1
-            code_p.tokens.append('    ' * code_p.indent + 'ptr--;\n')
-        elif cmd == '.':
-            code_p.tokens.append('    ' * code_p.indent + 'putchar(' + str(data[ptr]) + ');\n')
-        elif cmd == ',':
-            x = ord(getche())
-            code_p.tokens.append('    ' * code_p.indent + 'scanf("%u",&data[ptr]);\n')
-        elif cmd == '[':
-            code_p.tokens.append('    ' * code_p.indent + 'while(data[ptr]) {\n')
-            code_p.indent += 1
-        elif cmd == ']':
-            code_p.indent -= 1
-            code_p.tokens.append('    ' * code_p.indent + '}\n')
-
-        code_ptr += 1
-
-    return ''.join(code_p.tokens)
 
 def construct(ctx, source):
 
@@ -59,9 +16,9 @@ def construct(ctx, source):
     pilha = []
     enter = 0
     ctx.indent = 0
-    ctx.tokens.append('#include "stdio.h"\n')
-    ctx.tokens.append('#include "stdlib.h"\n')
-    ctx.tokens.append('int main() {\n')
+    ctx.tokens.append('#include <stdio.h>\n')
+    ctx.tokens.append('#include <stdlib.h>\n\n')
+    ctx.tokens.append('int main() {\n\n')
 
     ctx.indent = 1
 
@@ -90,52 +47,13 @@ def construct(ctx, source):
         elif cmd == '.':
             ctx.tokens.append('    ' * ctx.indent + 'putchar(data[ptr]);\n')
         elif cmd == ',':
-            x = input()
-            ctx.tokens.append('    ' * ctx.indent + 'scanf("%u",&data[ptr]);\n')
+            ctx.tokens.append('    ' * ctx.indent + 'scanf("%hhu",&data[ptr]);\n')
         elif cmd == '[':
-
-            if enter == 0:
-                armazena = code_ptr
-                while True:
-
-                    pilha.append(source[armazena])
-
-                    if source[armazena] == '[':
-                        op_b += 1
-
-                    if source[armazena] == ']':
-                        op_b -= 1
-
-                    if source[armazena] == ']' and op_b == 0:
-                        break;
-
-                    armazena += 1
-
-                while_code = ''.join(pilha)
-
-                code_pilha = n(code_p, while_code)
-
-                ctx.tokens.append(code_pilha)
-                pilha = []
-
-                code_p.tokens = []
-
-                enter = 1
-
-            if data[ptr] == 0:
-                open_brackets = 1
-                while open_brackets != 0:
-                    code_ptr += 1
-                    if source[code_ptr] == '[':
-                        open_brackets += 1
-                    elif source[code_ptr] == ']':
-                        open_brackets -= 1
-
-            else:
-                breakpoints.append(code_ptr)
+            ctx.tokens.append('    ' * ctx.indent + 'while(data[ptr]) {\n')
+            ctx.indent += 1
         elif cmd == ']':
-            # voltar para o colchete correspondente
-            code_ptr = breakpoints.pop() - 1
+            ctx.indent -= 1
+            ctx.tokens.append('    ' * ctx.indent + '}\n')
         else:
             enter = 0
 
