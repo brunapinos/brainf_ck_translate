61428a17db74c416f05d763c309beb43fa3f45bd
Bruna Pinos <bruna.pinos@gmail.com>
2017-11-08 21:48:34 -0200

    [UPDATE] Makes code more legible
    Signed-off-by: Bruna Pinos <bruna.pinos@gmail.com>
    Signed-off-by: Guilherme Augusto <guilherme.francais@gmail.com>

--- brainf_ck.py
+++ brainf_ck.py
@@ -3,65 +3,70 @@ from types import SimpleNamespace
 from getch import getche
 import click
 
-ctx = SimpleNamespace(tokens=[], indent=1)
+final_code = SimpleNamespace(tokens=[], indent=1)
 
-def construct(ctx, source):
+def construct(final_code, source_code):
 
     data = [0]
     ptr = 0
-    code_ptr = 0
+    counter = 0
     breakpoints = []
+    TAB = 9 # Tab in Ascii table
 
-    op_b = 0
-    pilha = []
     enter = 0
-    ctx.indent = 0
-    ctx.tokens.append('#include <stdio.h>\n')
-    ctx.tokens.append('#include <stdlib.h>\n\n')
-    ctx.tokens.append('int main() {\n\n')
+    final_code.indent = 0
+    final_code.tokens.append('#include <stdio.h>\n')
+    final_code.tokens.append('#include <stdlib.h>\n\n')
+    final_code.tokens.append('int main() {\n\n')
 
-    ctx.indent = 1
+    final_code.indent = 1
 
-    ctx.tokens.append('    ' * ctx.indent + 'unsigned char data[100000];\n')
-    ctx.tokens.append('    ' * ctx.indent + 'int ptr = 0;\n')
+    final_code.tokens.append(chr(TAB) * final_code.indent + \
+        'unsigned char data[100000];\n')
+    final_code.tokens.append(chr(TAB) * final_code.indent + 'int ptr = 0;\n')
 
-    while code_ptr < len(source):
-        cmd = source[code_ptr]
+    while counter < len(source_code):
+        character = source_code[counter]
 
-        if cmd == '+':
+        if character == '+':
             data[ptr] = (data[ptr] + 1) % 256
             x = ptr
-            ctx.tokens.append('    ' * ctx.indent + 'data[' + str(x) + '] += 1;\n')
-        elif cmd == '-':
+            final_code.tokens.append(chr(TAB) * final_code.indent + \
+                'data[' + str(x) + '] += 1;\n')
+        elif character == '-':
             data[ptr] = (data[ptr] - 1) % 256
             x = ptr
-            ctx.tokens.append('    ' * ctx.indent + 'data[' + str(x) + '] -= 1;\n')
-        elif cmd == '>':
+            final_code.tokens.append(chr(TAB) * final_code.indent + \
+                'data[' + str(x) + '] -= 1;\n')
+        elif character == '>':
             ptr += 1
-            ctx.tokens.append('    ' * ctx.indent + 'ptr++;\n')
+            final_code.tokens.append(chr(TAB) * final_code.indent + 'ptr++;\n')
             if ptr >= len(data):
                 data.append(0)
-        elif cmd == '<':
-            ctx.tokens.append('    ' * ctx.indent + 'ptr--;\n')
+        elif character == '<':
+            final_code.tokens.append(chr(TAB) * final_code.indent + 'ptr--;\n')
             ptr -= 1
-        elif cmd == '.':
-            ctx.tokens.append('    ' * ctx.indent + 'putchar(data[ptr]);\n')
-        elif cmd == ',':
-            ctx.tokens.append('    ' * ctx.indent + 'scanf("%hhu",&data[ptr]);\n')
-        elif cmd == '[':
-            ctx.tokens.append('    ' * ctx.indent + 'while(data[ptr]) {\n')
-            ctx.indent += 1
-        elif cmd == ']':
-            ctx.indent -= 1
-            ctx.tokens.append('    ' * ctx.indent + '}\n')
+        elif character == '.':
+            final_code.tokens.append(chr(TAB) * final_code.indent + \
+                'putchar(data[ptr]);\n')
+        elif character == ',':
+            final_code.tokens.append(chr(TAB) * final_code.indent + \
+                'scanf("%hhu",&data[ptr]);\n')
+        elif character == '[':
+            final_code.tokens.append(chr(TAB) * final_code.indent + \
+                'while(data[ptr]) {\n')
+            final_code.indent += 1
+        elif character == ']':
+            final_code.indent -= 1
+            final_code.tokens.append(chr(TAB) * final_code.indent + '}\n')
         else:
             enter = 0
 
-        code_ptr += 1
+        counter += 1
 
-    ctx.indent = 0
-    ctx.tokens.append('\n''    return 0;\n}')
-    return ''.join(ctx.tokens)
+    final_code.indent = 0
+    final_code.tokens.append('\n''    return 0;\n}')
+    return ''.join(final_code.tokens)
 
 
 @click.command()
@@ -74,15 +79,15 @@ def build(o, filename):
     enter_arq = open(filename, 'r')
 
     code = SimpleNamespace(tokens=[])
-    for linha in enter_arq:
-        code.tokens.append(linha)
+    for line in enter_arq:
+        code.tokens.append(line)
 
     enter_arq.close()
 
-    source = construct(ctx, ''.join(code.tokens))
+    source_code = construct(final_code, ''.join(code.tokens))
 
     exit_arq = open(output, 'w')
-    exit_arq.write(source)
+    exit_arq.write(source_code)
     exit_arq.close()
 
 if __name__ == '__main__':
